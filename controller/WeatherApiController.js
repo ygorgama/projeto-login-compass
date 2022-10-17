dfbe0641df2aab0a98a2ad9d4ebe182eeba93587
Ygor Gama <ygorpinto.gama@gmail.com>
2022-10-17 10:27:50 -0300

    Exibida na tela da página principal a sigla do estado e o nome da cidade

--- controller/WeatherApiController.js
+++ controller/WeatherApiController.js
@@ -12,6 +12,13 @@ class WeatherApiController {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
         let request = new XMLHttpRequest();
+        let secondRequest = new XMLHttpRequest();
+
+        secondRequest.open(
+          "GET",
+          `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&zoom=10`,
+          false
+        );
 
         request.open(
           "GET",
@@ -20,16 +27,114 @@ class WeatherApiController {
         );
         request.send();
 
-        const apiObj = JSON.parse(request.responseText);
-        console.log(apiObj.location.name);
+        secondRequest.send();
 
-        this._graus.innerHTML = apiObj.current.temp_c + "º";
-        this._icon.setAttribute("src", apiObj.current.condition.icon);
+        const locationApi = JSON.parse(secondRequest.responseText);
+
+        const wetherApi = JSON.parse(request.responseText);
+        console.log(locationApi);
+
+        this._graus.innerHTML = wetherApi.current.temp_c + "º";
+        this._icon.setAttribute("src", wetherApi.current.condition.icon);
         this._cidade.innerHTML =
-          apiObj.location.name + " - " + apiObj.location.region;
+          locationApi.address.city +
+          " - " +
+          this._templateState(wetherApi.location.region);
       },
       (err) => console.log(err),
       { enableHighAccuracy: true }
     );
+  }
+
+  _templateState(state) {
+    let stateInitials;
+    switch (state) {
+      case "Bahia":
+        stateInitials = "BA";
+        break;
+      case "Acre":
+        stateInitials = "AC";
+        break;
+      case "Alagoas":
+        stateInitials = "AL";
+        break;
+      case "Amapá":
+        stateInitials = "AP";
+        break;
+      case "Amazonas":
+        stateInitials = "AM";
+        break;
+      case "Ceará":
+        stateInitials = "CE";
+        break;
+      case "Distrito Federal":
+        stateInitials = "DF";
+        break;
+      case "Espírito Santo":
+        stateInitials = "ES";
+        break;
+      case "Goiás":
+        stateInitials = "GO";
+        break;
+      case "Maranhão":
+        stateInitials = "MA";
+        break;
+      case "Mato Grosso":
+        stateInitials = "MT";
+        break;
+      case "Mato Grosso do Sul":
+        stateInitials = "MS";
+        break;
+      case "Minas Gerais":
+        stateInitials = "MG";
+        break;
+      case "Pará":
+        stateInitials = "PA";
+        break;
+      case "Paraíba":
+        stateInitials = "PB";
+        break;
+      case "Paraná":
+        stateInitials = "PR";
+        break;
+      case "Pernambuco":
+        stateInitials = "PE";
+        break;
+      case "Piauí":
+        stateInitials = "PI";
+        break;
+      case "Rio de Janeiro":
+        stateInitials = "RJ";
+        break;
+      case "Rio Grande do Norte":
+        stateInitials = "RN";
+        break;
+      case "Rio Grande do Sul":
+        stateInitials = "RS";
+        break;
+      case "Rondônia":
+        stateInitials = "RO";
+        break;
+      case "Roraima":
+        stateInitials = "RR";
+        break;
+      case "Santa Catarina":
+        stateInitials = "SC";
+        break;
+      case "São Paulo":
+        stateInitials = "SP";
+        break;
+      case "Sergipe":
+        stateInitials = "SE";
+        break;
+      case "Tocantins":
+        stateInitials = "TO";
+        break;
+      default:
+        throw new Error("Some error ocurred");
+        break;
+    }
+
+    return stateInitials;
   }
 }
